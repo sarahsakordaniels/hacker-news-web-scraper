1cbccb55ce1f7376ef662194f08b0ac08cc2a9cc
sarahsakordaniels <ssdaniels13@gmail.com>
2020-02-28 04:57:46 -0700

    enable to scrape first two pages of articles

--- scraper.py
+++ scraper.py
@@ -3,12 +3,22 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
+
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+
 links = soup.select('.storylink')
+links2 = soup2.select('.storylink')
+
 subtext = soup.select('.subtext')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext + subtext2
 
 def sort_stories_by_votes(hackernewslist):
-    return sorted(hackernewslist, key=lambda k:k['votes'])
+    return sorted(hackernewslist, key=lambda k:k['votes'], reverse=True)
 
 def create_custom_hacker_news(links, subtext):
     hackernews = []
@@ -22,4 +32,4 @@ def create_custom_hacker_news(links, subtext):
                 hackernews.append({'title': title, 'link': href, 'votes': points})
     return sort_stories_by_votes(hackernews)
 
-pprint.pprint(create_custom_hacker_news(links, subtext))
+pprint.pprint(create_custom_hacker_news(mega_links, mega_subtext))
