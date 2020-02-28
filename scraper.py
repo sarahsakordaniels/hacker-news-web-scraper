8e7384ed43295410f83bd2e5c4b1798f8bee05d7
sarahsakordaniels <ssdaniels13@gmail.com>
2020-02-28 01:26:01 -0700

    print title, link, and score of stories with a score or 99 or greater

--- scraper.py
+++ scraper.py
@@ -1,8 +1,22 @@
 import requests 
 from bs4 import BeautifulSoup
-# beautiful soup allows us to use HTML
-# requests allows us to initially download that HTML
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
-# get request to grab this page 
-print(res)
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
+
+def create_custom_hacker_news(links, subtext):
+    hackernews = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href=item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace('points', ''))
+            if points > 99:
+                hackernews.append({'title': title, 'link': href, 'votes': points})
+    return hackernews
+
+pprint.pprint(create_custom_hacker_news(links, subtext))
